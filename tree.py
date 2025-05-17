a0f0ce9c39ee6ab9f697fa446080b1d4697f3236
Hemdy-M <hemdy.formation@gmail.com>
2025-05-17 14:55:29 +0200

    initial commit

---
+++ tree.py
@@ -0,0 +1,30 @@
+# Data structure : tree (binary tree)
+class BinaryTree:
+    def __init__(self, val: int, **kwargs) -> None:
+        self.val: int = val
+        try:
+            self.left: BinaryTree = kwargs.get("left")
+        except:
+            self.left: None = None
+        
+        try:
+            self.right: BinaryTree = kwargs.get("right")
+        except:
+            self.right: None = None
+        
+        self.output: list[int] = []
+
+    def preorderCheck(self, root) -> list[int]:
+        if root == None:
+            return self.output
+        
+        self.output.append(root.val)
+
+        if root.left != None:
+            self.preorderCheck(root.left)
+        if root.right != None:
+            self.preorderCheck(root.right)
+        return self.output
+
+root: BinaryTree = BinaryTree(0, left=BinaryTree(1), right=BinaryTree(2))
+print(root.preorderCheck(root))
